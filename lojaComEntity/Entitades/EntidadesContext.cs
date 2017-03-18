dcffd92780f1b2479512b78e15f01c3552e91890
Rafaela <rafaela.vm@gmail.com>
2017-03-18 20:06:43 -0300

    1. Primeiro commit

---
+++ lojaComEntity/Entitades/EntidadesContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lojaComEntity.Entitades
+{
+    public class EntidadesContext: DbContext
+    {
+        //mapeando a classe Usuario
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string stringConexao = ConfigurationManager.ConnectionStrings["lojaConnectionString"].ConnectionString;
+            optionsBuilder.UseSqlServer(stringConexao);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ lojaComEntity/Program.cs
@@ -0,0 +1,27 @@
+﻿using lojaComEntity.Entitades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lojaComEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntidadesContext contexto = new EntidadesContext();
+
+            Usuario usuario = new Usuario()
+            {
+                Nome = "Rafaela",
+                Senha = "123"
+            };
+
+            contexto.Usuarios.Add(usuario);
+            contexto.SaveChanges();
+            contexto.Dispose();
+        }
+    }
+}
